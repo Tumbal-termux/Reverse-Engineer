e375a22a9a3b6004d8742c399edbab4bea63d76b
Tumbal-termux <101085369+Tumbal-termux@users.noreply.github.com>
2022-03-10 15:56:19 +0700

    Update run.py

--- Punya_Latif/run.py
+++ Punya_Latif/run.py
@@ -1,1 +1,27 @@
 
+import optparse
+
+par = optparse.OptionParser(
+    usage = 'usage: %prog --bit 32 or --bit 64'
+)
+
+par.add_option(
+    '--bit',help='Masukan bit hp mu contoh: --bit 64',default=None
+)
+(arg,opt) = par.parse_args()
+
+if __name__ == "__main__":
+	if(arg.bit=="64"):
+		try:
+			from bit64 import main as co
+			co._response_output()
+		except Exception as e:
+			exit(e)
+	elif(arg.bit=="32"):
+		try:
+			from bit32 import main as cok
+			cok._response_output()
+		except Exception as e:
+			exit(e)
+	else:
+		exit("\n Gunakan python run.py --bit 64 Jika hpmu 64 Bit;\n Guanakan python run.py --bit 32 Jika hpmu 32 Bit.\n")
