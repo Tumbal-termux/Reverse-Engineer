4961a857b344348a7ef1e782ed24ba2bb83f4d3c
Tumbal-termux <101085369+Tumbal-termux@users.noreply.github.com>
2022-03-10 16:04:45 +0700

    Update run.py

--- Punya_Dumai-991/run.py
+++ Punya_Dumai-991/run.py
@@ -1,1 +1,9 @@
-
+import os
+if __name__ == "__main__":
+   try:
+       os.system("git pull")
+       __import__("X").Main_()._no_vpn()
+   except Exception as e:
+       exit(str(e))
+# Terima Kasih Kepada 
+# Mr.Risky, Jeeck, Ratu Error, Dan Grup Xnxcode
